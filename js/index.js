e3e22c373fc8a34054f1e5170d978087b458b167
yhc <haochaoying@126.com>
2016-02-22 00:46:38 +0800

    modification

--- js/index.js
+++ js/index.js
@@ -3,66 +3,63 @@ $(document).ready(function() {
 		verticalCentered: false,
 		paddingTop: 0,
 		resize: false,
-		anchors:['profile','work','resume','publications'],
+		anchors:['profile', 'experience', 'publication'],
 		scrollingSpeed: 400,
         afterLoad: function(anchorLink, index) {
             console.log(anchorLink);
             $("ul.nav li").removeClass("active");
             switch(anchorLink) {
-                case "work":
-                    $(".worknav").addClass("active");
+                case "profile":
+                    $(".profilenav").addClass("active");
                     break;
-                case "resume":
-                    $(".resumenav").addClass("active");
+                case "experience":
+                    $(".expernav").addClass("active");
                     break;
-                case "publications":
+                case "publication":
                     $(".pubsnav").addClass("active");
                     break;
-                    
             }
         }
 	});
-	forcePosition();
-	
-	$("#vmenu li").mouseover(function() {
-		$( this ).addClass("selected");
-		if ($( this ).attr("id") != "profilebutton")
-			$("#profilebutton").removeClass("selected");
-	});
-	
-	$("#vmenu li").mouseleave(function() {
-		$( this ).removeClass("selected");
-		$("#profilebutton").addClass("selected");
-	});
-	
+	// forcePosition();
+
+	// $("#vmenu li").mouseover(function() {
+	// 	$( this ).addClass("selected");
+	// 	if ($( this ).attr("id") != "profilebutton")
+	// 		$("#profilebutton").removeClass("selected");
+	// });
+
+	// $("#vmenu li").mouseleave(function() {
+	// 	$( this ).removeClass("selected");
+	// 	$("#profilebutton").addClass("selected");
+	// });
+
 	$("#profilebutton").click(function() {
 		$.fn.fullpage.moveTo('profile');
 	});
-	
-	$("#workbutton").click(function() {
-		$.fn.fullpage.moveTo('work');
+
+	$("#experbutton").click(function() {
+		$.fn.fullpage.moveTo('experience');
 	});
-	
-	$("#resumebutton").click(function() {
-		$.fn.fullpage.moveTo('resume');
-	});
-	
+
 	$("#pubbutton").click(function() {
-		$.fn.fullpage.moveTo('publications');
+		$.fn.fullpage.moveTo('publication');
 	});
+
+    // $("#blogbutton").click(function() {
+    //     $.fn.fullpage.moveTo('blog');
+    // });
 });
 
-$(window).resize(function() {
-	forcePosition();
-});
+// $(window).resize(function());
 
-// ensures that social links panel doesn't run into menu
-function forcePosition() {
-	//console.log ($("#sociallinks").offset());
-	
-	if ( $("#sociallinks").offset().top < 200 ) {
-		$("#sociallinks").addClass("moveup");
-	} else {
-		$("#sociallinks").removeClass("moveup");
-	}
-}
+// // ensures that social links panel doesn't run into menu
+// function forcePosition() {
+// 	//console.log ($("#sociallinks").offset());
+
+// 	if ( $("#sociallinks").offset().top < 200 ) {
+// 		$("#sociallinks").addClass("moveup");
+// 	} else {
+// 		$("#sociallinks").removeClass("moveup");
+// 	}
+// }
